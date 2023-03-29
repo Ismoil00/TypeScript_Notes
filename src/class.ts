3916f3875a9e767f141d519a905f3b6da4f6c552
Ismoil Sharifov <sharipovismoil01@gmail.com>
2023-03-29 22:44:53 +0500

    classes

--- src/class.ts
+++ src/class.ts
@@ -1,31 +1,31 @@
-
-// || CLASS:
-/* class User {
-  name: string;
-  email: string;
-  private city: string = "";
-  constructor(name: string, email: string){
-    this.name = name;
-    this.email = email;
-    this.city
+// || CLASS ||
+// |> Hardway of writing a class:
+class OldUser {
+  public readonly name: string;
+  private email: string;
+  protected age: number;
+  public status: string = "married";
+  constructor(ism: string, gmail: string, howOld: number) {
+    this.name = ism;
+    this.email = gmail;
+    this.age = howOld;
   }
-} */
+}
 
+// ------------------------------------------------------------------
+// |> Less-code way of writing a class:
 class Users {
-  // * By using 'protected'-keyword we make the variable avialable in the child classes besides parent class! 
-  protected courseCount: number = 1;
-
+  protected courseCount: number = 5;
   constructor(
-    public name: string, 
-    public email: string, 
-    private userID: string | number
-    ){
-  }
-  get getEmail():string {
+    public readonly name: string,
+    public email: string,
+    private userID: string | number = 1000
+  ) {}
+  public get getEmail(): string {
     return `this is the ${this.email}`;
   }
-  // * In TS setters do not accept any 'return declaration'!
-  set setEmail(newEmail: string) {
+  public set setEmail(newEmail: string) {
+    // * In TS setters do not accept any 'return declaration'!
     if (this.email === newEmail) {
       throw new Error("this email already exits!");
     }
@@ -34,17 +34,89 @@ class Users {
   private deleteEmail(): void {
     console.log(this.email);
   }
-}
-
-// * Class Inheritance;
-class subUser extends Users {
-  isFamily: boolean = true;
-  changeCourseCount(newCourseCount: number){
-    this.courseCount = newCourseCount;
+  protected simpleFunc(): string {
+    return "this is simple public function";
   }
 }
 
+class subUser extends Users {
+  isFamily: boolean = true;
+  constructor(name: string, email: string, public Class: string[]) {
+    super(name, email);
+    this.Class = Class;
+  }
+}
 
-const Ismoil = new Users("Ismoil", "ismoil@gmail.com", 125);
+const Ismoil = new Users("Ismoil", "ismoil@gmail.com");
+const Sorbon = new Users("Ismoil", "ismoil@gmail.com", 55510);
+const Umed = new subUser("Umed", "umed@gmail.com", ["math", "geometry"]);
+Umed.getEmail;
+Umed.simpleFunc();
 
-// Ismoil.deleteEmail();
+// ------------------------------------------------------------------
+// |> Class and Interface:
+
+interface Player {
+  name: string;
+  sportType: string;
+  train(hours: number): number;
+}
+
+class FootballPlayer implements Player {
+  name: string;
+  sportType: string;
+  constructor(val: string, type: string) {
+    this.name = val;
+    this.sportType = type;
+  }
+  train(hours: number): number {
+    return hours;
+  }
+}
+
+// ------------------------------------------------------------------
+// |> Class Statis Declaration
+
+class Peeps {
+  // * static declaration applies directly to the class but not to any specific object!
+  static count: number = 0;
+
+  static getCount(): number {
+    return Peeps.count;
+  }
+
+  public id: number;
+
+  constructor() {
+    this.id = ++Peeps.count;
+  }
+}
+
+// ------------------------------------------------------------------
+// |> GET and SET Methods:
+
+class Brand {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+      return;
+    }
+    throw new Error("Passed value is not an array of strings!");
+  }
+}
+
+const newBrand = new Brand();
+// SETTER
+newBrand.data = ["ismoil", "suhrob", "somon"];
+// GETTER
+console.log(newBrand.data);
